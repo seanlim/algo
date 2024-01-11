e4a4d02c93aa062f0aee330295aec17cea727024
Sean Lim <seanlimdev856@gmail.com>
2024-01-11 18:36:11 +0800

    add leetcode 43

---
+++ leetcode/43/Solution.py
@@ -0,0 +1,5 @@
+# Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+class Solution(object):
+    def multiply(self, num1, num2):
+        # LMAO
+        return str(eval(num1+'*'+num2))
